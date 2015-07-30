74d8ab9e5edfd120b464ff009b0e7af0055db96c
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2015-07-31 00:08:38 +0200

    Moved window icon resource to project resources file

--- SteamPlaceholder.Designer.vb
+++ SteamPlaceholder.Designer.vb
@@ -22,7 +22,6 @@ Partial Class SteamPlaceholder
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SteamPlaceholder))
         Me.openFileDialogBrowse = New System.Windows.Forms.OpenFileDialog()
         Me.lstCommands = New System.Windows.Forms.ListBox()
         Me.btnEnd = New System.Windows.Forms.Button()
@@ -32,24 +31,24 @@ Partial Class SteamPlaceholder
         Me.btnRemove = New System.Windows.Forms.Button()
         Me.btnSet = New System.Windows.Forms.Button()
         Me.btnTest = New System.Windows.Forms.Button()
-        Me.SuspendLayout()
+        Me.SuspendLayout
         '
         'openFileDialogBrowse
         '
-        Me.openFileDialogBrowse.AutoUpgradeEnabled = False
+        Me.openFileDialogBrowse.AutoUpgradeEnabled = false
         Me.openFileDialogBrowse.DefaultExt = "exe"
         Me.openFileDialogBrowse.InitialDirectory = "%ProgramFiles%"
-        Me.openFileDialogBrowse.ReadOnlyChecked = True
-        Me.openFileDialogBrowse.ShowHelp = True
+        Me.openFileDialogBrowse.ReadOnlyChecked = true
+        Me.openFileDialogBrowse.ShowHelp = true
         '
         'lstCommands
         '
-        Me.lstCommands.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.lstCommands.FormattingEnabled = True
-        Me.lstCommands.HorizontalScrollbar = True
-        Me.lstCommands.IntegralHeight = False
+        Me.lstCommands.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom)  _
+                        Or System.Windows.Forms.AnchorStyles.Left)  _
+                        Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.lstCommands.FormattingEnabled = true
+        Me.lstCommands.HorizontalScrollbar = true
+        Me.lstCommands.IntegralHeight = false
         Me.lstCommands.Location = New System.Drawing.Point(12, 12)
         Me.lstCommands.Name = "lstCommands"
         Me.lstCommands.Size = New System.Drawing.Size(369, 106)
@@ -57,29 +56,29 @@ Partial Class SteamPlaceholder
         '
         'btnEnd
         '
-        Me.btnEnd.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnEnd.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
         Me.btnEnd.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.btnEnd.Location = New System.Drawing.Point(493, 96)
         Me.btnEnd.Name = "btnEnd"
         Me.btnEnd.Size = New System.Drawing.Size(100, 23)
         Me.btnEnd.TabIndex = 23
         Me.btnEnd.Text = "Exit"
-        Me.btnEnd.UseVisualStyleBackColor = True
+        Me.btnEnd.UseVisualStyleBackColor = true
         '
         'btnAdd
         '
-        Me.btnAdd.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnAdd.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
         Me.btnAdd.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.btnAdd.Location = New System.Drawing.Point(387, 67)
         Me.btnAdd.Name = "btnAdd"
         Me.btnAdd.Size = New System.Drawing.Size(100, 23)
         Me.btnAdd.TabIndex = 24
         Me.btnAdd.Text = "Add (Duplicate)"
-        Me.btnAdd.UseVisualStyleBackColor = True
+        Me.btnAdd.UseVisualStyleBackColor = true
         '
         'txtArgs
         '
-        Me.txtArgs.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtArgs.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
         Me.txtArgs.Location = New System.Drawing.Point(387, 12)
         Me.txtArgs.Name = "txtArgs"
         Me.txtArgs.Size = New System.Drawing.Size(206, 20)
@@ -87,51 +86,51 @@ Partial Class SteamPlaceholder
         '
         'btnBrowse
         '
-        Me.btnBrowse.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnBrowse.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
         Me.btnBrowse.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.btnBrowse.Location = New System.Drawing.Point(493, 38)
         Me.btnBrowse.Name = "btnBrowse"
         Me.btnBrowse.Size = New System.Drawing.Size(100, 23)
         Me.btnBrowse.TabIndex = 26
         Me.btnBrowse.Text = "Browse..."
-        Me.btnBrowse.UseVisualStyleBackColor = True
+        Me.btnBrowse.UseVisualStyleBackColor = true
         '
         'btnRemove
         '
-        Me.btnRemove.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnRemove.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
         Me.btnRemove.Location = New System.Drawing.Point(387, 95)
         Me.btnRemove.Name = "btnRemove"
         Me.btnRemove.Size = New System.Drawing.Size(100, 23)
         Me.btnRemove.TabIndex = 27
         Me.btnRemove.Text = "Remove"
-        Me.btnRemove.UseVisualStyleBackColor = True
+        Me.btnRemove.UseVisualStyleBackColor = true
         '
         'btnSet
         '
-        Me.btnSet.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSet.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
         Me.btnSet.Location = New System.Drawing.Point(387, 38)
         Me.btnSet.Name = "btnSet"
         Me.btnSet.Size = New System.Drawing.Size(100, 23)
         Me.btnSet.TabIndex = 28
         Me.btnSet.Text = "<<== Set"
-        Me.btnSet.UseVisualStyleBackColor = True
+        Me.btnSet.UseVisualStyleBackColor = true
         '
         'btnTest
         '
-        Me.btnTest.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnTest.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
         Me.btnTest.Location = New System.Drawing.Point(493, 67)
         Me.btnTest.Name = "btnTest"
         Me.btnTest.Size = New System.Drawing.Size(100, 23)
         Me.btnTest.TabIndex = 29
         Me.btnTest.Text = "Run"
-        Me.btnTest.UseVisualStyleBackColor = True
+        Me.btnTest.UseVisualStyleBackColor = true
         '
         'SteamPlaceholder
         '
         Me.AcceptButton = Me.btnTest
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.AutoScroll = True
+        Me.AutoScroll = true
         Me.CancelButton = Me.btnEnd
         Me.ClientSize = New System.Drawing.Size(605, 130)
         Me.Controls.Add(Me.btnTest)
@@ -142,15 +141,14 @@ Partial Class SteamPlaceholder
         Me.Controls.Add(Me.btnAdd)
         Me.Controls.Add(Me.btnEnd)
         Me.Controls.Add(Me.lstCommands)
-        Me.HelpButton = True
-        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.HelpButton = true
+        Me.Icon = Global.SteamPlaceholder.My.Resources.Resources.steam
         Me.Name = "SteamPlaceholder"
         Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Steam Placeholder"
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
-
+        Me.ResumeLayout(false)
+        Me.PerformLayout
     End Sub
     Friend WithEvents openFileDialogBrowse As System.Windows.Forms.OpenFileDialog
     Friend WithEvents lstCommands As System.Windows.Forms.ListBox
