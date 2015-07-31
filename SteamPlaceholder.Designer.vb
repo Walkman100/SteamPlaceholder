a30dea2b7dfb03ce6353127e4fa5937ea18c28c4
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2015-07-31 02:02:25 +0200

    Converted lstCommands to a ListView, moved buttons around

--- SteamPlaceholder.Designer.vb
+++ SteamPlaceholder.Designer.vb
@@ -23,13 +23,15 @@ Partial Class SteamPlaceholder
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.openFileDialogBrowse = New System.Windows.Forms.OpenFileDialog()
-        Me.lstCommands = New System.Windows.Forms.ListBox()
+        Me.lstCommands = New System.Windows.Forms.ListView()
+        Me.colheadPath = New System.Windows.Forms.ColumnHeader()
+        Me.colheadProgramArgs = New System.Windows.Forms.ColumnHeader()
+        Me.colheadExtArgs = New System.Windows.Forms.ColumnHeader()
         Me.btnEnd = New System.Windows.Forms.Button()
         Me.btnAdd = New System.Windows.Forms.Button()
-        Me.txtArgs = New System.Windows.Forms.TextBox()
         Me.btnBrowse = New System.Windows.Forms.Button()
         Me.btnRemove = New System.Windows.Forms.Button()
-        Me.btnSet = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
         Me.btnTest = New System.Windows.Forms.Button()
         Me.SuspendLayout
         '
@@ -43,24 +45,45 @@ Partial Class SteamPlaceholder
         '
         'lstCommands
         '
+        Me.lstCommands.AllowColumnReorder = true
         Me.lstCommands.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom)  _
                         Or System.Windows.Forms.AnchorStyles.Left)  _
                         Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
-        Me.lstCommands.FormattingEnabled = true
-        Me.lstCommands.HorizontalScrollbar = true
-        Me.lstCommands.IntegralHeight = false
+        Me.lstCommands.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.colheadPath, Me.colheadProgramArgs, Me.colheadExtArgs})
+        Me.lstCommands.FullRowSelect = true
+        Me.lstCommands.GridLines = true
+        Me.lstCommands.HideSelection = false
+        Me.lstCommands.LabelEdit = true
         Me.lstCommands.Location = New System.Drawing.Point(12, 12)
+        Me.lstCommands.MultiSelect = false
         Me.lstCommands.Name = "lstCommands"
-        Me.lstCommands.Size = New System.Drawing.Size(369, 106)
+        Me.lstCommands.Size = New System.Drawing.Size(514, 168)
         Me.lstCommands.TabIndex = 21
+        Me.lstCommands.UseCompatibleStateImageBehavior = false
+        Me.lstCommands.View = System.Windows.Forms.View.Details
+        '
+        'colheadPath
+        '
+        Me.colheadPath.Text = "Program/file path"
+        Me.colheadPath.Width = 254
+        '
+        'colheadProgramArgs
+        '
+        Me.colheadProgramArgs.Text = "Program Arguments"
+        Me.colheadProgramArgs.Width = 105
+        '
+        'colheadExtArgs
+        '
+        Me.colheadExtArgs.Text = "Argument to use to start entry"
+        Me.colheadExtArgs.Width = 150
         '
         'btnEnd
         '
         Me.btnEnd.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
         Me.btnEnd.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.btnEnd.Location = New System.Drawing.Point(493, 96)
+        Me.btnEnd.Location = New System.Drawing.Point(532, 157)
         Me.btnEnd.Name = "btnEnd"
-        Me.btnEnd.Size = New System.Drawing.Size(100, 23)
+        Me.btnEnd.Size = New System.Drawing.Size(61, 23)
         Me.btnEnd.TabIndex = 23
         Me.btnEnd.Text = "Exit"
         Me.btnEnd.UseVisualStyleBackColor = true
@@ -69,28 +92,20 @@ Partial Class SteamPlaceholder
         '
         Me.btnAdd.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
         Me.btnAdd.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.btnAdd.Location = New System.Drawing.Point(387, 67)
+        Me.btnAdd.Location = New System.Drawing.Point(532, 12)
         Me.btnAdd.Name = "btnAdd"
-        Me.btnAdd.Size = New System.Drawing.Size(100, 23)
+        Me.btnAdd.Size = New System.Drawing.Size(61, 23)
         Me.btnAdd.TabIndex = 24
-        Me.btnAdd.Text = "Add (Duplicate)"
+        Me.btnAdd.Text = "Add"
         Me.btnAdd.UseVisualStyleBackColor = true
-        '
-        'txtArgs
-        '
-        Me.txtArgs.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
-        Me.txtArgs.Location = New System.Drawing.Point(387, 12)
-        Me.txtArgs.Name = "txtArgs"
-        Me.txtArgs.Size = New System.Drawing.Size(206, 20)
-        Me.txtArgs.TabIndex = 25
         '
         'btnBrowse
         '
         Me.btnBrowse.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
         Me.btnBrowse.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.btnBrowse.Location = New System.Drawing.Point(493, 38)
+        Me.btnBrowse.Location = New System.Drawing.Point(532, 99)
         Me.btnBrowse.Name = "btnBrowse"
-        Me.btnBrowse.Size = New System.Drawing.Size(100, 23)
+        Me.btnBrowse.Size = New System.Drawing.Size(61, 23)
         Me.btnBrowse.TabIndex = 26
         Me.btnBrowse.Text = "Browse..."
         Me.btnBrowse.UseVisualStyleBackColor = true
@@ -98,29 +113,29 @@ Partial Class SteamPlaceholder
         'btnRemove
         '
         Me.btnRemove.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
-        Me.btnRemove.Location = New System.Drawing.Point(387, 95)
+        Me.btnRemove.Location = New System.Drawing.Point(532, 41)
         Me.btnRemove.Name = "btnRemove"
-        Me.btnRemove.Size = New System.Drawing.Size(100, 23)
+        Me.btnRemove.Size = New System.Drawing.Size(61, 23)
         Me.btnRemove.TabIndex = 27
         Me.btnRemove.Text = "Remove"
         Me.btnRemove.UseVisualStyleBackColor = true
         '
-        'btnSet
+        'btnEdit
         '
-        Me.btnSet.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
-        Me.btnSet.Location = New System.Drawing.Point(387, 38)
-        Me.btnSet.Name = "btnSet"
-        Me.btnSet.Size = New System.Drawing.Size(100, 23)
-        Me.btnSet.TabIndex = 28
-        Me.btnSet.Text = "<<== Set"
-        Me.btnSet.UseVisualStyleBackColor = true
+        Me.btnEdit.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.btnEdit.Location = New System.Drawing.Point(532, 70)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(61, 23)
+        Me.btnEdit.TabIndex = 33
+        Me.btnEdit.Text = "Edit"
+        Me.btnEdit.UseVisualStyleBackColor = true
         '
         'btnTest
         '
         Me.btnTest.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
-        Me.btnTest.Location = New System.Drawing.Point(493, 67)
+        Me.btnTest.Location = New System.Drawing.Point(532, 128)
         Me.btnTest.Name = "btnTest"
-        Me.btnTest.Size = New System.Drawing.Size(100, 23)
+        Me.btnTest.Size = New System.Drawing.Size(61, 23)
         Me.btnTest.TabIndex = 29
         Me.btnTest.Text = "Run"
         Me.btnTest.UseVisualStyleBackColor = true
@@ -132,12 +147,11 @@ Partial Class SteamPlaceholder
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoScroll = true
         Me.CancelButton = Me.btnEnd
-        Me.ClientSize = New System.Drawing.Size(605, 130)
+        Me.ClientSize = New System.Drawing.Size(605, 192)
         Me.Controls.Add(Me.btnTest)
-        Me.Controls.Add(Me.btnSet)
+        Me.Controls.Add(Me.btnEdit)
         Me.Controls.Add(Me.btnRemove)
         Me.Controls.Add(Me.btnBrowse)
-        Me.Controls.Add(Me.txtArgs)
         Me.Controls.Add(Me.btnAdd)
         Me.Controls.Add(Me.btnEnd)
         Me.Controls.Add(Me.lstCommands)
@@ -148,16 +162,17 @@ Partial Class SteamPlaceholder
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Steam Placeholder"
         Me.ResumeLayout(false)
-        Me.PerformLayout
     End Sub
-    Friend WithEvents openFileDialogBrowse As System.Windows.Forms.OpenFileDialog
-    Friend WithEvents lstCommands As System.Windows.Forms.ListBox
-    Friend WithEvents btnEnd As System.Windows.Forms.Button
-    Friend WithEvents btnAdd As System.Windows.Forms.Button
-    Friend WithEvents txtArgs As System.Windows.Forms.TextBox
-    Friend WithEvents btnBrowse As System.Windows.Forms.Button
-    Friend WithEvents btnRemove As System.Windows.Forms.Button
-    Friend WithEvents btnSet As System.Windows.Forms.Button
-    Friend WithEvents btnTest As System.Windows.Forms.Button
+    Private colheadExtArgs As System.Windows.Forms.ColumnHeader
+    Private colheadProgramArgs As System.Windows.Forms.ColumnHeader
+    Private colheadPath As System.Windows.Forms.ColumnHeader
+    Private openFileDialogBrowse As System.Windows.Forms.OpenFileDialog
+    Private WithEvents lstCommands As System.Windows.Forms.ListView
+    Private btnEnd As System.Windows.Forms.Button
+    Private btnAdd As System.Windows.Forms.Button
+    Private btnBrowse As System.Windows.Forms.Button
+    Private btnRemove As System.Windows.Forms.Button
+    Private btnEdit As System.Windows.Forms.Button
+    Private btnTest As System.Windows.Forms.Button
 
 End Class
--- SteamPlaceholder.vb
+++ SteamPlaceholder.vb
@@ -3,21 +3,20 @@ Imports System.IO
 Imports System.Threading
 
 Public Class SteamPlaceholder
-
+    
     Private Sub LoadSteamPlaceHolder() Handles Me.Load
         ' fill gui: lstCommands.Items = My.Settings.Paths
-        For i = 0 To My.Settings.Paths.Count - 1
+        'For i = 0 To My.Settings.Paths.Count - 1
             'lstCommands.Items.Add(My.Settings.Paths.Item(i))
-        Next
-        lstCommands.SelectedIndex = 0
-
+        'Next
+        
         'get CommandLineArgs and apply/run them
         For Each s As String In My.Application.CommandLineArgs
             If s.ToLower = "hidegui" Then
                 WindowState = FormWindowState.Minimized
             Else
                 Try
-                    lstCommands.SelectedIndex = s
+                    'lstCommands.SelectedIndex = s
                     RunProgram()
                 Catch ex As Exception
                     MsgBox("""" & s.ToString & """ is not a valid integer or doesn't exist in the list!" & vbNewLine & vbNewLine & _
@@ -26,60 +25,38 @@ Public Class SteamPlaceholder
             End If
         Next
     End Sub
-
+    
     Private Sub EndSPH() Handles btnEnd.Click
         Application.Exit()
     End Sub
-
-    Private Sub lstCommands_Changed() Handles lstCommands.Click, lstCommands.SelectedIndexChanged, lstCommands.SelectedValueChanged
-        txtArgs.Text = lstCommands.SelectedItem
+    
+    Private Sub CheckButtons() Handles lstCommands.Click, lstCommands.SelectedIndexChanged
+        
     End Sub
-
-    Private Sub SaveStrings()
-        'My.Settings.Paths = lstCommands.Items
-        'My.Settings.Paths.Clear()
-        For i = 0 To lstCommands.Items.Count - 1
-            'My.Settings.Paths.Add(lstCommands.Items.Item(i))
-        Next
-    End Sub
-
-    Private Sub SetItem() Handles btnSet.Click
-        lstCommands.Items.Item(lstCommands.SelectedIndex) = txtArgs.Text
-        SaveStrings()
-    End Sub
-
+    
     Private Sub Browse() Handles btnBrowse.Click
-        If openFileDialogBrowse.ShowDialog() = Windows.Forms.DialogResult.OK Then
-            txtArgs.Text = openFileDialogBrowse.FileName
-            SetItem()
+        If openFileDialogBrowse.ShowDialog() = DialogResult.OK Then
+            lstCommands.FocusedItem.Text = openFileDialogBrowse.FileName
         End If
     End Sub
-
+    
     Private Sub RunProgram() Handles btnTest.Click
         Try
-            Process.Start(lstCommands.SelectedItem)
+            Process.Start(lstCommands.FocusedItem.Text)
         Catch ex As Exception
-            MsgBox("There was an error running the program '" & lstCommands.SelectedItem & "!", MsgBoxStyle.Critical)
+            MsgBox("There was an error running the program '" & lstCommands.FocusedItem.Text & "!", MsgBoxStyle.Critical)
         End Try
     End Sub
-
+    
     Private Sub AddItem() Handles btnAdd.Click
-        If lstCommands.Items.Count = 1 Then
-            btnRemove.Enabled = True
-        End If
-        lstCommands.Items.Insert(lstCommands.SelectedIndex + 1, txtArgs.Text)
-        lstCommands.SelectedIndex = lstCommands.SelectedIndex + 1
+        lstCommands.Items.Add("")
     End Sub
-
+    
     Private Sub RemoveItem() Handles btnRemove.Click
-        If lstCommands.SelectedIndex = -1 Then
+        If IsNothing(lstCommands.FocusedItem) Then
             MsgBox("No item selected")
         Else
-            lstCommands.SelectedIndex = lstCommands.SelectedIndex - 1
-            lstCommands.Items.RemoveAt(lstCommands.SelectedIndex + 1)
-        End If
-        If lstCommands.Items.Count = 1 Then
-            btnRemove.Enabled = False
+            lstCommands.FocusedItem.Remove
         End If
     End Sub
 End Class
